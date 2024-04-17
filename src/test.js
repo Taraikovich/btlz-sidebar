fe887b60e00f19f4160305a627c95fe3ed61b68f
Taraikovich <71258577+Taraikovich@users.noreply.github.com>
2024-04-17 11:10:00 +0400

    feat: test 3

--- src/test.js
+++ src/test.js
@@ -1,4 +1,7 @@
 export function test() {
     console.log(document.URL);
     console.log(document.baseURI);
+    google.script.run.withSuccessHandler((data) => {
+        console.log(data);
+    }).getID();
 }
